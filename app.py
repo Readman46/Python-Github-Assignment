79fcf4e8359dacd0eb6143cc75f2163886d10927
Readman46 <cbryant105@usf.edu>
2025-11-24 20:27:38 -0500

    Add files via upload
    Add formatted output displaying calculation results

--- app.py
+++ app.py
@@ -4,3 +4,5 @@ amount = input("How much money did you make today? $")
 
 amount = float(amount)
 weekly_total = amount * 7
+
+print(f"If you make this amount every day, you'll make ${weekly_total:.2f} this week!")
